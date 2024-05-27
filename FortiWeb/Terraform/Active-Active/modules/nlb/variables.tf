0967bd1df1ac902ef066ea1e48a3855c876d5509
Ozan Oguz <61783699+ozanoguz@users.noreply.github.com>
2024-05-27 14:58:48 +0300

    Add files via upload

---
+++ FortiWeb/Terraform/Active-Active/modules/nlb/variables.tf
@@ -0,0 +1,17 @@
+// variables.tf
+
+variable "forwarding_config" {}
+
+variable "subnet_id" {}
+
+variable "subnet2_id" {}
+
+variable "vpc_id" {}
+
+variable "target1_id" {}
+
+variable "target2_id" {}
+
+variable "healthport" {}
+
+variable "prefix" {}
